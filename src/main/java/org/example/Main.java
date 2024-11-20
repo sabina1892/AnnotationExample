670c5bc6693b0c2d0c466fbe85f3574efaf3a91e
Sabina Mammadova <sabina1892m@gmail.com>
2024-11-20 17:15:17 +0400

    Annotation added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,68 @@
 package org.example;
-import java.math.BigInteger;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
 import java.util.*;
-
-
-public class Main {
-
-    public static void main(String[] args) {
-        HashSet<String> hs = new HashSet<>();
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        sc.nextLine();
-        for(int i =0;i<n;i++){
-            String s = sc.nextLine().trim().toLowerCase();
-            if(s.length()<=10)  hs.add(s);
-            System.out.println(hs.size());
-        }
-
-
-    }
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface FamilyBudget {
+    String userRole() default "GUEST";
+    int budgetLimit();
 }
+
+class FamilyMember {
+    @FamilyBudget(userRole = "SENIOR", budgetLimit = 200)
+    public void seniorMember(int budget, int moneySpend) {
+        System.out.println("Senior Member");
+        System.out.println("Spend: " + moneySpend);
+        System.out.println("Budget Left: " + (budget - moneySpend));
+    }
+    @FamilyBudget(userRole = "JUNIOR", budgetLimit = 200)
+    public void juniorUser(int budget, int moneySpend) {
+        System.out.println("Junior Member");
+        System.out.println("Spend: " + moneySpend);
+        System.out.println("Budget Left: " + (budget - moneySpend));
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String role = in.next();
+            int spend = in.nextInt();
+            in.nextLine();
+            try {
+                Class annotatedClass = FamilyMember.class;
+                Method[] methods = annotatedClass.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(FamilyBudget.class)) {
+                        FamilyBudget family = method
+                                .getAnnotation(FamilyBudget.class);
+                        String userRole = family.userRole();
+                        int budgetLimit = family.budgetLimit();
+                        if (userRole.equals(role)) {
+                            if(spend<=budgetLimit){
+                                method.invoke(FamilyMember.class.newInstance(),
+                                        budgetLimit, spend);
+                            }else{
+                                System.out.println("Budget Limit Over");
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            testCases--;
+        }
+    }
+
+
+}
+
+
